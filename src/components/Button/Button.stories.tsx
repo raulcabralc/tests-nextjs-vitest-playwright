472d8bbcaa4a02e6ed576cc09f43a40731d0fd87
Raul Cabral <raulcabralc@gmail.com>
2025-07-28 17:08:42 -0300

    feat: storybook initial Button config

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,18 @@
+import { Button } from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Playground: Story = {
+  args: {
+    children: 'Texto do botão',
+    variant: "default",
+    size: "md"
+  },
+};
