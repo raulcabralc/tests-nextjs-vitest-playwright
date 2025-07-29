fd2312341f9266fe5bf1a08c7004bd1b2f345ba3
Raul Cabral <raulcabralc@gmail.com>
2025-07-29 14:32:04 -0300

    chore: add front end tests and components

---
+++ src/core/tests/mocks/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "@/core/todo/schemas/todo.contract";
+
+export const mockTodos: Todo[] = [
+  {
+    id: "1746472743268",
+    task: "Fazer café",
+    createdAt: "2025-05-04T20:11:13.279Z",
+  },
+  {
+    id: "1746472935131",
+    task: "Estudar para a prova",
+    createdAt: "2025-05-05T17:28:01.564Z",
+  },
+  {
+    id: "1746472957750",
+    task: "Ler O Senhor Dos Anéis",
+    createdAt: "2025-05-01T15:51:46.876Z",
+  },
+];
